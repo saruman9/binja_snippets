09d6a2335f70ec2936265c31d0b9eaed973a168b
saruman9 <rum.274.4@gmail.com>
2025-02-25 00:03:46 +0800

    feat(flow_rename): rename/define global symbols

--- flow_rename.py
+++ flow_rename.py
@@ -12,15 +12,16 @@ from binaryninja.highlevelil import (
     HighLevelILVar,
     HighLevelILConstPtr,
     HighLevelILVarInit,
+    HighLevelILAssign,
 )
 from binaryninja.function import Function
 from binaryninja.interaction import show_message_box
-from binaryninja.enums import MessageBoxIcon, TypeClass
+from binaryninja.enums import MessageBoxIcon, TypeClass, SymbolType
 from binaryninja.log import log_error, log_alert, log_debug
 from binaryninja.exceptions import ILException
 from binaryninja.variable import CoreVariable, Variable
-from binaryninja.types import CoreSymbol
-from binaryninjaui import HighlightTokenState, UIContext
+from binaryninja.types import Symbol
+from binaryninjaui import HighlightTokenState, UIContext  # type: ignore[import-untyped]
 
 if typing.TYPE_CHECKING:
     from binaryninja.binaryview import BinaryView
@@ -80,13 +81,16 @@ def format_name(input: str) -> str:
     return input
 
 
-def find_name(instruction: HighLevelILInstruction) -> str | None:
+def find_name(instruction: HighLevelILInstruction) -> str | bool | None:
     log_debug(
         f"{find_name.__name__}: {instruction} ({type(instruction)}) at {instruction.address:#x}"
     )
     match instruction:
         case HighLevelILConstPtr(constant=address):
             symbol = bv.get_symbol_at(address)
+            if not symbol:
+                log_error(f"Can't get symbol at {address:#x}")
+                return False
             return symbol.name
         case HighLevelILVar(var=var):
             return var.name
@@ -103,8 +107,16 @@ def find_variable(
         case HighLevelILVar(var=var):
             if var.core_variable == core_var:
                 return var
-        case _:
-            return None
+    return None
+
+
+def find_global(instruction: HighLevelILInstruction) -> int | None:
+    log_debug(
+        f"{find_global.__name__}: {instruction} ({type(instruction)}) at {instruction.address:#x}"
+    )
+    match instruction:
+        case HighLevelILConstPtr(constant=address):
+            return address
     return None
 
 
@@ -126,11 +138,17 @@ def rename_variable_without_collisions(variable: Variable, name: str, index: int
     function_vars = variable.function.vars
     for local_var in function_vars:
         if local_var.name == name:
-            rename_variable_without_collisions(
-                variable, f"{name}_{index}", index + 1
-            )
+            rename_variable_without_collisions(variable, f"{name}_{index}", index + 1)
             return
     variable.name = name
+
+
+def rename_global_without_collisions(address: int, name: str, index: int = 1):
+    while bv.get_symbols_by_name(name):
+        rename_global_without_collisions(address, f"{name}_{index}", index + 1)
+        return
+    symbol = Symbol(SymbolType.DataSymbol, address, name)
+    bv.define_user_symbol(symbol)
 
 
 def set_name(
@@ -163,8 +181,17 @@ def set_name(
             rename_variable_without_collisions(target_var, target_param.name)
             return True
         case HighLevelILVarInit(dest=target_var, src=src_instruction):
-            name = next(src_instruction.traverse(find_name))
+            name = next(src_instruction.traverse(find_name))  # type: ignore[arg-type]
+            if not name:
+                return False
             rename_variable_without_collisions(target_var, format_name(name))
+            return True
+        case HighLevelILAssign(dest=target_instruction, src=src_instruction):
+            name = next(src_instruction.traverse(find_name))  # type: ignore[arg-type]
+            if not name:
+                return False
+            target_address = next(target_instruction.traverse(find_global))  # type: ignore[arg-type]
+            rename_global_without_collisions(target_address, f"g_{format_name(name)}")
             return True
     return None
 
